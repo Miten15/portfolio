74fcf6d71d2021172a8fef663ed3b9842b73cc1e
Miten <miten2006@gmail.com>
2024-06-23 21:44:20 +0530

    done

--- components/Mask.tsx
+++ components/Mask.tsx
@@ -3,18 +3,20 @@ import { MaskContainer } from "./ui/svg-mask-effect";
 
 const Mask = () => {
   return (
-    <div className="h-full w-full  items-center justify-center  overflow-hidden">
+    <div className="h-full w-full flex items-center justify-center overflow-hidden">
       <MaskContainer
         revealText={
-          <p className="max-w-4xl mx-auto text-slate-800 text-center  text-4xl font-bold my-10 ">
-            Hi, I'm a very lazy dev & My favorite framework is Copy-Paste.js.
+          <p className="max-w-4xl mx-auto text-slate-800 text-center text-4xl font-bold my-10">
+            Hi, I&apos;m a very creative dev &amp; My favorite framework is{" "}
+            <span className="text-purple">Next.js.</span>
           </p>
         }
         className="h-[40rem] border rounded-md"
       >
-        Hi, I'm a very <span className="text-purple">creative</span>
-        {""} dev & My favorite framework is {""}
-        <span className="text-purple">Next.js.</span>
+        <p className="text-2xl text-gray-700">
+          Hi, I&apos;m a very creative dev &amp; My favorite framework is{" "}
+          <span className="text-purple">Next.js.</span>
+        </p>
       </MaskContainer>
     </div>
   );
